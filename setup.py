c3136b990c021840f9e046be10167d9f622d6de7
Timotej Kapus <timotej.kapus13@imperial.ac.uk>
2017-11-20 06:36:03 +0000

    Increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='tosheets',
-    version='0.1.8',
+    version='0.2.0',
     author='Timotej Kapus',
     author_email='kren1@users.noreply.github.com',
     description=('Cmd utility that send stdin to google sheets'),
